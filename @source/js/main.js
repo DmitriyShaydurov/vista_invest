5e2aa4dd35d67898bb50269b8a6efc31aaf3f991
Shaydurov Dmitriy <dmitriy.shaydurov@gmail.com>
2020-09-21 10:29:57 +0700

    locations added

--- @source/js/main.js
+++ @source/js/main.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+
+$(function () {
+    $('#navbarDropdown a').click(function (e) {
+        $('.active').removeClass('active');
+    });
+});
+
+// $('.collapse').on('shown.bs.collapse', function(e) {
+//     var $card = $(this).closest('.card');
+//     $('html,body').animate({
+//       scrollTop: $card.offset().top
+//     }, 500);
+//   });
+
+
+// function scroll() {
+//   var elmnt = document.getElementById("carouselExampleIndicators-2");
+//   elmnt.scrollIntoView();
+// }
+
+$('.collapse').on('shown.bs.collapse', function(e) {
+    console.log(e.currentTarget.parentNode);
+    var elmnt = e.currentTarget.parentNode.parentNode;
+    // elmnt.scrollTop = elmnt.scrollHeight - 1000;
+    elmnt.scrollIntoView({behavior: "smooth", block: "start"});
+  });
